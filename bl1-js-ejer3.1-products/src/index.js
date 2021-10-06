d55fa3ab37aa845e7db83a090c46bfa8aeadb092
Manu <manu@cipfpbatoi.es>
2021-10-06 16:47:16 +0200

    acabado

--- bl1-js-ejer3.1-products/src/index.js
+++ bl1-js-ejer3.1-products/src/index.js
@@ -1,32 +1,129 @@
 'use strict'
 
-const Store = require("./store.class");
+const Store = require('./store.class')
+
+const myStore = new Store(1)
+
+const prod1 = {
+    name: 'TV Samsung MP45',
+    price: 345.95,
+    units: 3,
+}
+
+const prod2 ={
+    name: 'Ábaco de madera',
+    price: 245.95,
+    units: 0
+}
+const prod3 = {
+    name: 'impresora Epson LX-455',
+    price: 45.95,
+    units: 0
+}
+const prod4 = {
+    name: 'USB Kingston 16GB',
+    price: 5.95,
+    units: 45
+}
+    
+    //añade los productos
+    let TV = myStore.addProduct(prod1)
+    let abaco = myStore.addProduct(prod2)
+    let impresora = myStore.addProduct(prod3)
+    let USB = myStore.addProduct(prod4)
+
+    let cloneTV = TV
+    let cloneAbaco = abaco
+    let cloneImpresora = impresora
+    let cloneUSB = USB
+
+    //cambia en TV el precio por 325.90 y sus unidades pasarán a ser 8
+try{
+
+    cloneTV.price = 325.90
+    myStore.changeProductUnits(cloneTV.id,+5)
+    myStore.changeProduct(cloneTV)
+
+}catch(error){
+    console.error(error)
+}
+
+//al ábaco le sumamos 15 uds 
+try{
+
+    myStore.changeProductUnits(cloneAbaco.id,+15)
+    myStore.changeProduct(cloneAbaco)
+
+}catch(error){
+    console.error(error)
+}
+
+//a la impresora le ponemos de precio 55.90 y de unidades -2
+try{
+    
+    cloneImpresora.price = 55.90
+    myStore.changeProductUnits(cloneImpresora.id,-2);
+    myStore.changeProduct(cloneImpresora)
+
+}catch(error){
+    console.error(error)
+}
+
+//a la TV le restamos 10 unidades
+try{
+    
+    myStore.changeProductUnits(cloneTV.id,-10)
+    myStore.changeProduct(cloneTV)
+
+}catch(error){
+    console.error(error)
+}
+
+//al ábaco le cambiamos el nombre por 'Ábaco de madera (nuevo modelo)'
+try{
+    
+    cloneAbaco.name = 'Ábaco de madera (nuevo modelo)'
+    myStore.changeProduct(cloneAbaco)
+
+}catch(error){
+    console.error(error)
+}
+
+//mostrar por consola el almacén
+    console.log(myStore.toString())
+
+//mostrar por consola el texto 'LISTADO DEL ALMACÉN alfabético'
+    console.log('\nLISTADO DEL ALMACÉN alfabético')
+    console.log(myStore.orderByName().toString())
+
+//eliminar la TV
+try{
+    myStore.delProduct(cloneTV.id)
+    console.log(cloneTV.toString())
+}catch(error){
+    console.error(error)
+}
+   
+//eliminar la impresora
+try{
+    myStore.delProduct(cloneImpresora.id)
+    console.log(cloneImpresora.toString())
+}catch(error){
+    console.error(error)
+}
+    
+//mostrar por consola el almacén ordenado por existencias
+    console.log('\nLISTADO DEL ALMACÉN por existencias')
+    console.log(myStore.orderByUnits().toString()+ '\n')  
+
+//mostrar por consola los productos del almacén con menos de 10 unidades
+    console.log('\nLISTADO DE PRODUCTOS CON POCAS EXISTENCIAS')
+    console.log(myStore.underStock(10).toString() + '\n')
 
 // Creamos un nuevo almacén con id 1
 // Antes deberás haber importado la 
 // clase Store para poder usarla
 
-const myStore = new Store(1);
-
-const prod1 ={
-    name: 'tv',
-    price: 399.95,
-    units:3
-}
-
-const prod2 ={
-    name: 'tv',
-    price: 399.95,
-    units:3
-}
-
-try{
-    let product1 = myStore.addProduct(prod1)
-    myStore.delProduct(product1.id)
-}catch(error){
-    console.error(error);
-}
-myStore.findProduct(1);
 // Añadimos los 4 objetos que nos piden
 
 // Hacemos las 5 modificaciones
@@ -36,5 +133,6 @@ myStore.findProduct(1);
 
 // Eliminamos los 2 productos
 
-// Mostramos por consola todo lo que nos piden
+// Mostramos por consola todo lo que nos pide
 
+module.exports = Store;
--- bl1-js-ejer3.1-products/src/store.class.js
+++ bl1-js-ejer3.1-products/src/store.class.js
@@ -8,63 +8,123 @@ class Store {
     }
 
     findProduct(id){
-        return this.products.find(producto => producto.id === id)       
+        return this.products.find(producte => producte.id === id)
     }
 
-    addProduct(product){
-        if (!product.name){
-            throw 'ERROR no se le pasa name'
-        } 
-        if(!product.price){
-            throw 'Error no hay un price'
+
+    addProduct(producto){
+        if(!producto.name){
+            throw 'El producto no contiene nombre'
         }
-        if (!isNaN(product.price)){
-            if (product.price < 0){
-                throw 'ERROR no se le pasa price o no es un número positivo'
-            } 
-        } else {
-            throw 'Error'
+        if(!producto.price || producto.price < 0){
+            throw 'El precio es incorrecto'
         }
-        if (product.units){
-            if (product.units < 0 || !Number.isInteger(product.units)){
-                throw 'ERROR se le pasa units pero no es un número entero positivo'
+        if(producto.units){
+            if(!Number.isInteger(producto.units)){
+                throw 'Las unidades deben ser numericas'
+            }
+            if(producto.units < 0){
+                throw 'Las unidades son incorrectas'
             }
         }
-        let newProduct = new Product(this.maxId()+1,product.name,product.price,product.units)
-        this.products.push(newProduct)
-        return newProduct;
+        if(!Number(producto.price)){
+            throw 'El precio debe ser numerico'
+        }
+        const newProd = new Product(
+            this.maxId() + 1,
+            producto.name,
+            producto.price,
+            producto.units
+        )
+        this.products.push(newProd)
+        return newProd
     }
-    
+
     maxId(){
-        return this.products.reduce((max,product) => product.id > max ? product.id : max, 0);
+        return this.products.reduce((max,producte) => producte.id > max ? producte.id : max,0)
     }
 
     delProduct(id){
-        let producto = this.findProduct(id)
-        if(!producto || producto.units <= 0){
-            throw 'error'
-        } else {
-            let index = this.products.findIndex(prod => prod.id === id)
-            return this.products.splice(index,0);
+        let product = this.findProduct(id)
+        if(product === undefined){
+            throw 'El producto no existe'
+        }else if(product.units != 0){
+            throw 'Las unidades no estan a 0'
+        }else{
+            let index = this.products.findIndex(producte => producte.id === id)
+            return this.products.splice(index,0)
         }
     }
-
-    changeProduct(product){
-        let productoId = this.findProduct(product.id)
-        if(product.name){
-            productoId.name = product.name
+    changeProduct(data){
+        let product = this.findProduct(data.id)
+        if(data.name){
+            product.name = data.name
         }
-        if(product.price){
-            productoId.price = product.price
+        if(data.price){
+            product.price = data.price
         }
-        if(product.units){
-            productoId.units = product.units
+        if(data.units){
+            product.units = data.units
         }
-        return productoId
+        return product
     }
-
+    
     changeProductUnits(id, units){
-        
+        let product = this.findProduct(id)
+        if(!product){
+            throw 'El id no corresponde a ningun producto'
+        }
+        let calculo = product.units+units
+        if(calculo < 0){
+            throw 'Las unidades son incorrectas'
+        }else{
+            product.units = calculo
+            return product
+        }
+    }
+
+    totalImport(){
+        let importe = this.products.reduce((total,precio) => total += precio.productImport(), 0);    // total = 35.35
+        return importe
+    }
+    underStock(units){
+        let productos = []
+        for(let i = 0; i < this.products.length; i++){
+            let producto = this.products[i]
+            if(producto.units < units){
+                productos.push(producto)
+            }
+        }
+        return productos
+    }
+    orderByUnits(){
+        let productosOrdenados=this.products.sort(function(producto1, producto2) {
+            return producto1.units-producto2.units;
+          });
+          return productosOrdenados
+    }
+
+    orderByName(){
+        let productosOrdenados=this.products.sort((producto1, producto2) => producto1.name.localeCompare(producto2.name));
+        return productosOrdenados
+    }
+
+    toString(){
+        let cadena = 'Almacen ' + this.id + ' => ' + this.products.length + ' productos: ' + this.totalImport().toFixed(2) + ' €\n'
+        for(let i = 0; i < this.products.length; i++){
+            let prod = this.products[i].toString() + '\n'
+            cadena = cadena + ' - ' + prod
+        }
+        return cadena
+    }
+
+    toRender(product){
+        let cadena = 'Almacen ' + this.id + ' => ' + product.length + ' productos: ' + this.totalImport().toFixed(2) + ' €\n'
+        for(let i = 0; i < product.length; i++){
+            let prod = product[i].toString() + '\n'
+            cadena = cadena + ' - ' + prod
+        }
+        return cadena
     }
 }
 // y tendrás que exportarla
